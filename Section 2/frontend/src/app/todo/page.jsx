5ea255327dec4d1ac98aa453a6355aef09905ba1
m-triple-m <triplem656@gmail.com>
2024-04-15 15:30:24 +0530

    complete todo

--- Section 2/frontend/src/app/todo/page.jsx
+++ Section 2/frontend/src/app/todo/page.jsx
@@ -3,20 +3,32 @@ import React, { useState } from 'react'
 
 const Todo = () => {
 
-    const [todoList, setTodoList] = useState([
-        { task: 'Do homework', completed: false },
-        { task: 'Eat Food', completed: false },
-        { task: 'Play Games', completed: false },
-        { task: 'Sleep', completed: false },
-    ]);
+    const [todoList, setTodoList] = useState([]);
 
     const addTask = (e) => {
 
         if (e.code === 'Enter') {
             console.log(e.target.value);
+
+            setTodoList([...todoList, { task: e.target.value, completed: false }]);
+            e.target.value = '';
         }
 
     }
+
+    const deleteTask = (index) => {
+        console.log(index);
+        const temp = todoList;
+        temp.splice(index, 1);
+        setTodoList([...temp]);
+    }
+
+    const finishTask = (index) => {
+        const temp = todoList;
+        temp[index].completed = !temp[index].completed;
+        console.log(temp);
+        setTodoList([...temp]);
+    } 
 
     return (
         <div className='container py-5'>
@@ -31,9 +43,24 @@ const Todo = () => {
                 <div className="card-body">
                     {
                         todoList.map((item, index) => {
-                            return <div key={index} className='d-flex justify-content-between p-3'>
+                            return <div key={index} className='d-flex justify-content-between align-items-center p-3'>
                                 <p>{item.task}</p>
-                                <button className='btn btn-danger'>Delete</button>
+
+                                { item.completed ? 
+                                    <span className='badge bg-success'>Completed</span>
+                                    : 
+                                    <span className='badge bg-warning'>Pending</span>
+                                }
+
+                                <div>
+                                    <button onClick={() => { finishTask(index) }} className='btn btn-primary me-3'>
+                                        {
+                                            item.completed ? 'Undo' : 'Finish'
+                                        }
+                                    </button>
+                                    <button onClick={() => { deleteTask(index) }} className='btn btn-danger'>Delete</button>
+                                </div>
+                            
                             </div>
                         })
                     }
