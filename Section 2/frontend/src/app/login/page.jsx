6fa79dae1ba04835111cd52c4212059e049a9de4
m-triple-m <triplem656@gmail.com>
2024-07-02 16:11:38 +0530

    login

--- Section 2/frontend/src/app/login/page.jsx
+++ Section 2/frontend/src/app/login/page.jsx
@@ -1,13 +1,50 @@
+'use client';
 import React from 'react';
 import classes from './login.module.css';
 import icon from './myimage.jpeg';
+import { useFormik } from 'formik';
 
 const Login = () => {
+
+    const loginForm = useFormik({
+        initialValues: {
+            email: '',
+            password: ''
+        },
+        onSubmit: (values) => {
+            console.log(values);
+        }
+    })
+
     return (
-        <div>
-            <h1>Login</h1>
-            <button className={classes.loginBtn}>Login Button</button>
-            <img src={icon.src} alt="" />
+        <div className='col-md-4 mx-auto'>
+
+            <div className='card shadow'>
+                <div className='card-body'>
+                    <h3 className='text-center py-4'>Login Form</h3>
+                    <form onSubmit={loginForm.handleSubmit}>
+                        <label htmlFor="">Email Address</label>
+                        <input type="email"
+                            className='form-control mb-5'
+                            id='email'
+                            onChange={loginForm.handleChange}
+                            value={loginForm.values.email} />
+
+                        <label htmlFor="">Password</label>
+                        <input
+                            type="password"
+                            className='form-control mb-5'
+                            id='password'
+                            onChange={loginForm.handleChange}
+                            value={loginForm.values.password}
+                        />
+
+                        <button type='submit' className='btn btn-primary mt-5 w-100'>Submit</button>
+                    </form>
+
+                </div>
+            </div>
+
         </div>
     )
 }
--- Section 2/frontend/src/app/manage-users/page.jsx
+++ Section 2/frontend/src/app/manage-users/page.jsx
@@ -1,5 +1,7 @@
 'use client';
+import Link from 'next/link';
 import React, { useEffect, useState } from 'react'
+import toast from 'react-hot-toast';
 
 const ManageUsers = () => {
 
@@ -18,6 +20,16 @@ const ManageUsers = () => {
         fetchUsers();
     }, [])
 
+    const deleteUser = async (id) => {
+        const res = await fetch(`http://localhost:5000/user/delete/${id}`, {
+            method: 'DELETE'
+        });
+        console.log(res.status);
+        if (res.status === 200) {
+            toast.success('User deleted successfully');
+            fetchUsers();
+        }
+    }
 
     return (
         <div className='pt-5'>
@@ -34,6 +46,7 @@ const ManageUsers = () => {
                             <th>Name</th>
                             <th>Email</th>
                             <th>Created At</th>
+                            <th colSpan={2}>Actions</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -43,7 +56,16 @@ const ManageUsers = () => {
                                     <td>{user._id}</td>
                                     <td>{user.name}</td>
                                     <td>{user.email}</td>
-                                    <td>{user.createdAt}</td>
+                                    <td>{new Date(user.createdAt).toDateString()}</td>
+                                    <td>
+                                        <Link href={'/update-user/'+user._id} className='btn btn-primary'>Edit</Link>
+                                    </td>
+                                    <td>
+                                        <button
+                                            onClick={() => { deleteUser(user._id) }}
+                                            className='btn btn-danger'
+                                        >Delete</button>
+                                    </td>
                                 </tr>
                             })
                         }
